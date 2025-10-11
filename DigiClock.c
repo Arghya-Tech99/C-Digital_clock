39348444acf3ce3fde4d0283c739994fc1d71be1
Arghya-Tech99 <sinhaarghya03@gmail.com>
2025-10-12 02:05:27 +0530

    Print time in user-defined format
    Takes input from the user, and then prints time as per user's demand- either in 24 hour or in 12 hour (Which is default format)

--- DigiClock.c
+++ DigiClock.c
@@ -1,16 +1,42 @@
 #include<stdio.h>
 #include<time.h>
+#include<string.h>
+
+void getTime(char*, int);
+int format_Choice(); 
 
 int main() {
+
+    int choice = format_Choice();
+    char time[50];
+
+    getTime(time, choice);
+
+    printf("Current time : %s", time); // Prints the current local time
+    return 0;
+}
+
+void getTime(char* buffer, int format) {
+
     time_t rawTime; // declares a variable named rawTime of the data type time_t
     struct tm *currentTime; // Declaring a structure name 'tm' and a pointer to this structure with the name 'currentTime'
-    
-    char buffer[30];
-
     time(&rawTime); // get the current calendar time and store that value in "rawTime" variable
     currentTime = localtime(&rawTime); // Converts time value stored in "rawTime" into a local time format and assigns to structure
-    strftime(buffer, sizeof(buffer), "%H: %M: %S", currentTime); // Formats the time data type into a string variable
+    
+    if (format == 1) {
+    strftime(buffer, 50, "%H:%M:%S", currentTime); // Formats the time data type into a string variable; displays in 24 hour fomrat
+    } else {
+    strftime(buffer, 50, "%I:%M:%S %p", currentTime); // Formats the time data type into a string variable; displays 12 hour fomrat
+    }
+}
 
-    printf("Cureent time : %s", buffer); // Prints the current local time
-    return 0;
+int format_Choice() {
+    int format;
+    printf("Please enter the format in which you want the time to be displayed");
+    printf("\n 1. 24 hour format");
+    printf("\n 2. 12 hour format (Default)");
+    printf("\n Enter your choice(1/2): ");
+    scanf("%d", &format);
+
+    return format;
 }
